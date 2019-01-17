4d8fc258e63512030b601e7740f12f82159f7d99
polymythic <steve.struebing@gmail.com>
2019-01-16 21:51:18 -0500

    adding wire and node classes

---
+++ electric_node.py
@@ -0,0 +1,7 @@
+class electric_node():
+
+    def __init__(self, id, description):
+        self.id = id
+        self.description = description
+        wires_connected = [] #list of wires
+       
---
+++ electric_wire.py
@@ -0,0 +1,7 @@
+class electric_wire():
+
+    def __init__(self, id, prev_node, next_node):
+        self.id = id
+        self.prev_node = prev_node
+        self.next_node = next_node
+        
